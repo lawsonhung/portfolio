fdd046af8a234689bf37ebf1b83b433bbe18dd3b
lawsonhung <lawsonhung@yahoo.com>
2020-06-22 20:25:19 -0400

    Added text shadow to name

--- src/About.js
+++ src/About.js
@@ -23,7 +23,7 @@ class About extends Component {
         </Row>
         <Row className='justify-content-md-center'>
           <Col xs={12} sm={4} md={4}>
-            <Image src={ProfilePic} roundedCircle />
+            <Image className='profile-pic' src={ProfilePic} roundedCircle />
           </Col>
         </Row>
       </div>
