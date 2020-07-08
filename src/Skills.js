6917c6e9abdab6b0117fac1832edb2dd11008745
lawsonhung <lawsonhung@yahoo.com>
2020-07-07 22:36:08 -0400

    Refactored Skills

--- src/Skills.js
+++ src/Skills.js
@@ -17,75 +17,75 @@ class Skills extends Component {
 
   render() {
     return (
-      <div>
+      <div id='skills'>
         <div className='skills-divider'>&nbsp;</div>
 
-        <div className='skills' id='skills'>
-        <Container className='row1'>
-          <Row>
+        <div className='skills'>
+          <Container className='row1'>
+            <Row>
 
-            <Col className='flip-card'>
-              <div className='flip-card-inner'>
-                <img src={ReactLogo} className='logo' alt='React' />
-              </div>
-            </Col>
-            
-            <Col className='flip-card'>
-              <div className='flip-card-inner'>
-                <img src={ReduxLogo} className='logo' alt='Redux'/>
-              </div>
-            </Col>
+              <Col className='flip-card'>
+                <div className='flip-card-inner'>
+                  <img src={ReactLogo} className='logo' alt='React' />
+                </div>
+              </Col>
+              
+              <Col className='flip-card'>
+                <div className='flip-card-inner'>
+                  <img src={ReduxLogo} className='logo' alt='Redux'/>
+                </div>
+              </Col>
 
-            <Col className='flip-card'>
-              <div className='row1col3'>
-                <img src={JavaScriptLogo} className='logo' alt='JavaScript'/>
-              </div>
-            </Col>
-          </Row>
+              <Col className='flip-card'>
+                <div className='row1col3'>
+                  <img src={JavaScriptLogo} className='logo' alt='JavaScript'/>
+                </div>
+              </Col>
+            </Row>
 
-          <Row className='language'>
-            <Col><p>React</p></Col>
-            <Col><p>Redux</p></Col>
-            <Col><p>JavaScript</p></Col>
-          </Row>
+            <Row className='language'>
+              <Col><p>React</p></Col>
+              <Col><p>Redux</p></Col>
+              <Col><p>JavaScript</p></Col>
+            </Row>
 
-        </Container>
+          </Container>
 
-        <Container>
-          <Row>
-            
-            <Col className='flip-card'>
-              <div className='row2'>
-                <img src={RailsLogo} className='logo' alt='Ruby on Rails'/>
-              </div>
-            </Col>
+          <Container>
+            <Row>
+              
+              <Col className='flip-card'>
+                <div className='row2'>
+                  <img src={RailsLogo} className='logo' alt='Ruby on Rails'/>
+                </div>
+              </Col>
 
-            <Col className='flip-card'>
-              <div className='row2'>
-                <img src={HTMLLogo} className='logo' alt='HTML5'/>
-              </div>
-            </Col>
+              <Col className='flip-card'>
+                <div className='row2'>
+                  <img src={HTMLLogo} className='logo' alt='HTML5'/>
+                </div>
+              </Col>
 
-            <Col className='flip-card'>
-              <div className='row2'>
-                <img src={CSSLogo} className='logo' alt='CSS3'/>
-              </div>
-            </Col>
-            
-            <Col className='flip-card'>
-              <div className='row2'>
-                <img src={PostgreSQLLogo} className='logo' alt='PostgreSQL'/>
-              </div>
-            </Col>
-          </Row>
+              <Col className='flip-card'>
+                <div className='row2'>
+                  <img src={CSSLogo} className='logo' alt='CSS3'/>
+                </div>
+              </Col>
+              
+              <Col className='flip-card'>
+                <div className='row2'>
+                  <img src={PostgreSQLLogo} className='logo' alt='PostgreSQL'/>
+                </div>
+              </Col>
+            </Row>
 
-          <Row className='language'>
-            <Col><p>Ruby on Rails</p></Col>
-            <Col><p>HTML5</p></Col>
-            <Col><p>CSS3</p></Col>
-            <Col><p>PostgreSQL</p></Col>
-          </Row>
-        </Container>
+            <Row className='language'>
+              <Col><p>Ruby on Rails</p></Col>
+              <Col><p>HTML5</p></Col>
+              <Col><p>CSS3</p></Col>
+              <Col><p>PostgreSQL</p></Col>
+            </Row>
+          </Container>
         </div>
       </div>
     )
