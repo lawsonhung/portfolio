54f9f02c82fb384a7ce01634fccb0ae02ed31f3c
lawsonhung <lawsonhung@yahoo.com>
2020-07-26 17:49:42 -0400

    Attempt to fix Projects CSS on mobile

--- src/Projects.js
+++ src/Projects.js
@@ -12,7 +12,7 @@ class Projects extends Component {
         <div className='projects-divider'>&nbsp;</div>
 
         <div className='projects'>
-          <Container>
+          {/* <Container>
             <Row>
               <Col>
 
@@ -62,9 +62,58 @@ class Projects extends Component {
 
               </Col>
             </Row>
-          </Container>
+          </Container> */}
+
+          <div className='grid-wrapper'>
+
+            <div className='card'>
+              <h1>Delightful Texts</h1>
+
+              <iframe title='Delightful Texts' width="560" height="315" src="https://www.youtube.com/embed/vMZTea1-GBA" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+              <span className='project-links'>
+                <a href='https://github.com/lawsonhung/delightful-texts-frontend' target='_blank' rel='noopener noreferrer'>Frontend</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href='https://github.com/lawsonhung/Delightful-Texts-Backend' target='_blank' rel='noopener noreferrer'>Backend</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href='http://delightful-texts.surge.sh/registration' target='_blank' rel='noopener noreferrer'>Project</a>
+              </span>
+            </div>
+
+            <div className='card'>
+
+              <h1>Let's Chat</h1>
+
+              <iframe title="Let's Chat" width="560" height="315" src="https://www.youtube.com/embed/fzxA1PBSZeU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+              <span className='project-links'>
+                <a href='https://github.com/lawsonhung/ChatApp-FrontEnd' target='_blank' rel='noopener noreferrer'>Frontend</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href='https://github.com/lawsonhung/ChatApp-BackEnd' target='_blank' rel='noopener noreferrer'>Backend</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href='http://angry-cabbage.surge.sh/' target='_blank' rel='noopener noreferrer'>Project</a>
+              </span>
+            </div>
+
+            <div className='card'>
+
+              <h1>Java</h1>
+
+              <iframe title='Java' width="560" height="315" src="https://www.youtube.com/embed/Rz_69f4Z_4w" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+              <span className='project-links'>
+                <a href='https://github.com/aleksandra1995/coffee-review-app/tree/master/coffee-review-front-end' target='_blank' rel='noopener noreferrer'>Frontend</a>
+                <span>&nbsp;|&nbsp;</span>
+                <a href='https://github.com/aleksandra1995/coffee-review-app/tree/master/coffee-reviews-backend/mod-3-project-coffee-reviews' target='_blank' rel='noopener noreferrer'>Backend</a>
+              </span>
+            </div>
+
+          {/* End .grid-wrapper */}
+          </div>
+          {/* End .projects */}
         </div>
 
+      {/* End #projects */}
       </div>
     )
   }
